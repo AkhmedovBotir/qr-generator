7a2f7a737dd1d2e7d569fd2cf1908f0d1e433c76
Akhmedov Botir <134036244+AkhmedovBotir@users.noreply.github.com>
2023-07-24 16:39:22 +0500

    news

--- src/Main.js
+++ src/Main.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import './style.css'
 const Main = () => {
      const [temp, setTemp] = useState("");
-     const [word, setWord] = useState("");
+     const [word, setWord] = useState("https://akhmedov-botir.uz");
      const [qrCode, setQrCode] = useState("");
 
      useEffect(() => {
@@ -14,30 +14,27 @@ const Main = () => {
      }
 
      return (
-          <div className='d-flex flex-column justify-content-center align-items-center' style={{height: '100vh'}}>
-               <h1 className="text-light text-uppercase my-3">QR Code generator</h1>
-               <div className="w-100 py-3 d-flex justify-content-center">
-                    <input 
+          <div className="body">
+               <div className="container">
+                    <input
                          type="text"
                          placeholder="Matn yoki link yozing..."
-                         className="form-control w-25"
-                         onChange={(e) => {setTemp(e.target.value)}}
+                         className="bodyInput"
+                         onChange={(e) => { setTemp(e.target.value) }}
                     />
-               </div>
-               <div className="output">
-                    <img src={qrCode} alt="" />
-               </div>
-               <div className="w-25 d-flex justify-content-center py-3">
-                    <button 
-                         className="btn btn-danger px-3 mx-3"
-                         onClick={handleGenerate}
-                    >
-                         Generate
-                    </button>
-                    <a href={qrCode} download="QRCode"  className='btn btn-primary mx-3'>
-                         <button className="bg-transparent border-0 text-light" type="button">Download</button>
-                    </a>
-                    
+                    <div className="bodyContent">
+                         <button
+                              className="btn btn-success"
+                              onClick={handleGenerate}
+                         >
+                              Generate
+                         </button>
+                         <a href={qrCode} download="QRCode" className=''>
+                              <button className="btn btn-primary" type="button">Download</button>
+                         </a>
+
+                    </div>
+                    <img src={qrCode} className="qrImage" />
                </div>
           </div>
      );
